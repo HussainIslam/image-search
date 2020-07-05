bb239dd7e9a365403def85da6b626023033dd463
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-07-05 16:56:07 -0400

    carousel working

--- src/Components/images/images.jsx
+++ src/Components/images/images.jsx
@@ -1,21 +1,62 @@
-import React, { Component } from "react";
+import React, { Component, useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
-class Images extends Component {
-    render() {
-        const data = this.props.data
-            ? this.props.data.map((item) => {
-                  const container = {};
-                  container.src = item.urls.thumb;
-                  container.width = item.width;
-                  container.height = item.height;
-                  return container;
-              })
-            : null;
-        return data ? (
-            <Gallery photos={data} direction={"column"} columns={6} />
-        ) : null;
-    }
-}
+const Images = (props) => {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+
+    const data = props.data
+        ? props.data.map((item) => {
+              const container = {};
+              container.src = item.urls.thumb;
+              container.width = item.width;
+              container.height = item.height;
+              container.large = item.urls.regular;
+              return container;
+          })
+        : null;
+    const customStyles = {
+        view: (base) => ({
+            ...base,
+            height: "90vh",
+        }),
+    };
+    return (
+        <div>
+            {data ? (
+                <Gallery
+                    photos={data}
+                    direction={"column"}
+                    columns={6}
+                    onClick={openLightbox}
+                />
+            ) : null}
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            styles={customStyles}
+                            views={data.map((x) => ({
+                                source: x.large,
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+        </div>
+    );
+};
 
 export default Images;
