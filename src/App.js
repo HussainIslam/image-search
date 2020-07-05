94b9f6f847c653372b469eec599ca1aa0522d206
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-07-05 01:04:00 -0400

    added semantec ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Layout from "./Layout/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                </Switch>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/nav-bar/nav-bar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div>
+            <p>This is navbar</p>
+        </div>
+    );
+};
+
+export default NavBar;
