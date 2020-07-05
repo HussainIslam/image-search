7b264d0eb53914aaff01cee5bb76908a0db69579
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-07-05 05:57:28 -0400

    change from semantic to bootstra

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { InputGroup, FormControl } from "react-bootstrap";
 
-const Home = () => {
-    return (
-        <div>
-            <h1>HOME</h1>
-            <p>This is home page</p>
-        </div>
-    );
-};
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchString: "",
+        };
+    }
+
+    handleSearch(event) {
+        this.setState({ searchString: event.target.value }, () =>
+            console.log(this.state.searchString)
+        );
+    }
+
+    render() {
+        return (
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="Search images here"
+                    aria-label="Search bar for images"
+                    onChange={this.handleSearch.bind(this)}
+                />
+                <InputGroup.Append>
+                    <InputGroup.Text>Search</InputGroup.Text>
+                </InputGroup.Append>
+            </InputGroup>
+        );
+    }
+}
 
 export default Home;
